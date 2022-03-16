fb6ce78dd3a76e540abc153f6e285f5b1c393729
ww98 <418060258@qq.com>
2022-03-16 17:53:19 +0800

    fix slideCart

--- components/SlideCart/index.js
+++ components/SlideCart/index.js
@@ -36,7 +36,7 @@ export default function SlideCart(props) {
     <Transition.Root show={props.open} as={Fragment}>
       <Dialog
         as="div"
-        className="fixed inset-0 overflow-hidden"
+        className="fixed inset-0 z-50 overflow-hidden"
         onClose={props.setOpen}
       >
         <div className="absolute inset-0 overflow-hidden">
